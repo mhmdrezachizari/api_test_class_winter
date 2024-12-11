0ae0f670c554c74c8cf02cad9d40db4a2c0d36dc
mhmdreza <mhmdrezachizari@gmail.com>
2024-12-11 11:21:29 +0330

    new

--- product/urls.py
+++ product/urls.py
@@ -1,9 +1,12 @@
 from django.urls import include , path
 from django.contrib import admin
-from .views import ProductList ,ProductDetail
+from .views import ProductList, ProductDetail, singleProduct
+
 urlpatterns = [
     path('' , ProductList.as_view()),
     path('detail/', ProductDetail.as_view()),
+
+    path('single/<int:pk>' , singleProduct.as_view()),
 
 
 ]
--- product/views.py
+++ product/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
 # Create your views here.
@@ -22,3 +22,13 @@ class ProductDetail(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class singleProduct(APIView):
+    def get(self , request , pk):
+        product = get_object_or_404(Product, pk=pk)
+        if product is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        else:
+            serializer = ProductSerializer(product)
+            return Response(serializer.data , status=status.HTTP_200_OK)
